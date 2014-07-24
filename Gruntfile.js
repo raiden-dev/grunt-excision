470ebd937a2ef356ed934e9274a65988d6eaa70c
Andrey Kravtsov <raidendev@gmail.com>
2014-07-24 19:48:47 +0400

    Set testing environment

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          require: 'coverage/blanket'
+        },
+        src: ['test/**/*.js']
+      },
+      coverage: {
+        options: {
+          reporter: 'html-cov',
+          quiet: true,
+          captureFile: 'coverage/coverage.html'
+        },
+        src: ['test/**/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('default', 'mochaTest');
+  grunt.registerTask('tests', 'mochaTest');
+
+};
